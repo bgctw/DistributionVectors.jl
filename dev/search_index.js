7fa9c4f51992b9d86814e38bb76a06040a340ded
Documenter.jl <documenter@juliadocs.github.io>
2021-03-17 15:15:52 +0100

    build based on b881ce3

--- dev/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DistributionVectors","category":"page"},{"location":"#DistributionVectors","page":"Home","title":"DistributionVectors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DistributionVectors]","category":"page"},{"location":"#DistributionVectors.DistributionVectors","page":"Home","title":"DistributionVectors.DistributionVectors","text":"\n\n\n\n","category":"module"}]
-}
